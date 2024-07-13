f5ccabcf227122df7d7de5d40bf38078000e89f9
Anirudh331 <anianuj4@gmail.com>
2024-07-13 14:42:17 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,20 +190,8 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
+/*Tawk*/
 
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/669173c532dca6db2cae8df6/1i2k2ofkm';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-// End of Tawk.to Live Chat
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
